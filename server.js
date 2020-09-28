ca23620ef9a94d639bfdd689a7cc8ae1db908316
bregarulai <bregarulai@gmail.com>
2020-09-28 18:42:42 -0400

    Basic implementation of register route

--- server.js
+++ server.js
@@ -40,6 +40,19 @@ app.post("/signin", (req, res) => {
   }
 });
 
+app.post("/register", (req, res) => {
+  const { email, name, password } = req.body;
+  database.users.push({
+    id: "125",
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date(),
+  });
+  res.json(database.users[database.users.length - 1]);
+});
+
 app.listen(3001, () => {
   console.log("App is running on port 3001");
 });
