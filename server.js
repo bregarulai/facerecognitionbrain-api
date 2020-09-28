2baef3d40985875aff5a20ee3378c2ae9461f6e5
bregarulai <bregarulai@gmail.com>
2020-09-28 18:17:02 -0400

    Created fake database for now, also created signin route skeleton

--- server.js
+++ server.js
@@ -2,10 +2,43 @@ const express = require("express");
 
 const app = express();
 
+const database = {
+  users: [
+    {
+      id: "123",
+      name: "john",
+      email: "john@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: "124",
+      name: "sally",
+      email: "sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date(),
+    },
+  ],
+};
+
 app.get("/", (req, res) => {
-  res.send("This is working");
+  res.json("This is working");
+});
+
+app.post("/signin", (req, res) => {
+  res.json("signed in");
 });
 
 app.listen(3001, () => {
   console.log("App is running on port 3001");
 });
+
+/* 
+    / --> GET = this is working
+    /signin --> POST = success/fail
+    /register --> POST = user
+    /profile/:userId --> GET = user
+    /image --> PUT = updated user
+*/
