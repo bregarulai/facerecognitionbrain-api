727d25d136da785b2a505092dba4775b5b1eb8cc
bregarulai <bregarulai@gmail.com>
2020-10-02 23:29:28 -0400

    Updated register end point to store hash and email in login table

--- server.js
+++ server.js
@@ -78,22 +78,30 @@ app.post("/signin", (req, res) => {
 
 app.post("/register", (req, res) => {
   const { email, name, password } = req.body;
-  /*
-  bcrypt.hash(password, null, null, function (err, hash) {
-    // Store hash in your password DB.
-  });
-  */
-  db("users")
-    .returning("*")
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json("Unable to register."));
+  const hash = bcrypt.hashSync(password);
+  db.transaction((trx) => {
+    trx
+      .insert({
+        email: email,
+        hash: hash,
+      })
+      .into("login")
+      .returning("email")
+      .then((loginEmail) => {
+        return trx("users")
+          .returning("*")
+          .insert({
+            email: loginEmail[0],
+            name: name,
+            joined: new Date(),
+          })
+          .then((user) => {
+            res.json(user[0]);
+          });
+      })
+      .then(trx.commit)
+      .catch(trx.rollback);
+  }).catch((err) => res.status(400).json("Unable to register."));
 });
 
 app.get("/profile/:id", (req, res) => {
