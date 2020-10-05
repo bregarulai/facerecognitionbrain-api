14d42d678a3d08bc2382dd3322194c19ea2075de
bregarulai <bregarulai@gmail.com>
2020-10-05 12:42:42 -0400

    Implemented signin end point with database information

--- server.js
+++ server.js
@@ -16,64 +16,36 @@ const db = Knex({
 });
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
-
-const database = {
-  users: [
-    {
-      id: "123",
-      name: "john",
-      email: "john@gmail.com",
-      password: "cookies",
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: "124",
-      name: "sally",
-      email: "sally@gmail.com",
-      password: "bananas",
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-  login: [
-    {
-      id: "987",
-      hash: "",
-      email: "john@gmail.com",
-    },
-    {
-      id: "123",
-      hash: "",
-      email: "sally@gmail.com",
-    },
-  ],
-};
-
 app.get("/", (req, res) => {
-  res.json(database.users);
+  res.json("Success.");
 });
 
 app.post("/signin", (req, res) => {
-  /*
-  bcrypt.compare("bacon", hash, function (err, res) {
-    // res == true
-  });
-  bcrypt.compare("veggies", hash, function (err, res) {
-    // res = false
-  });
-  */
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json("Error login in!");
-  }
+  db.select("email", "hash")
+    .from("login")
+    .where("email", "=", req.body.email)
+    .then((data) => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if (isValid) {
+        return db
+          .select("*")
+          .from("users")
+          .where("email", "=", req.body.email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => {
+            res.status(400).json("Unable to get user.");
+          });
+      } else {
+        res.status(400).json("Wrong credentials.");
+      }
+    })
+    .catch((err) => {
+      res.status(400).json("Wrong credentials.");
+    });
 });
 
 app.post("/register", (req, res) => {
